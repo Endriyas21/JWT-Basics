65dbe00ac29e1139471ebe41417d82dd07c599d1
Endriyas <endriyas@uni.minerva.edu>
2024-05-31 19:17:09 +0300

    errors

---
+++ errors/bad-request.js
@@ -0,0 +1,11 @@
+const customAPIError = require("./cu");
+const { StatusCodes } = require("http-status-codes");
+
+class BadRequest extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+module.exports = BadRequest;
---
+++ errors/index.js
@@ -0,0 +1,5 @@
+const BadRequest = require("./bad-request");
+const Unauthenticated = require("./unauthenticated");
+const CustomAPIError = require("./custom-error");
+
+module.exports = { BadRequest, Unauthenticated, CustomAPIError };
